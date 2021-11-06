4ac0a23b4055a1c05424dea1f87e642aa5640181
ivan.cherkasov <ivan.cherkasov@quadcode.com>
2021-11-06 05:18:52 +0300

    hw04_lru_cache done

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,5 +1,7 @@
 package hw04lrucache
 
+import "sync"
+
 type Key string
 
 type Cache interface {
@@ -9,8 +11,7 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
+	mu       sync.Mutex
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
@@ -27,4 +28,57 @@ func NewCache(capacity int) Cache {
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
 	}
+}
+
+func (l *lruCache) Set(key Key, value interface{}) bool {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	item, ok := l.items[key]
+	cItem := cacheItem{key: key.string(), value: value}
+
+	if ok {
+		item.Value = cItem
+		l.queue.MoveToFront(item)
+
+		return true
+	}
+
+	item = l.queue.PushFront(cItem)
+
+	if l.queue.Len() > l.capacity {
+		last := l.queue.Back()
+		cache := last.Value.(cacheItem)
+		l.queue.Remove(last)
+
+		delete(l.items, Key(cache.key))
+	}
+
+	l.items[key] = l.queue.Front()
+
+	return false
+}
+
+func (l *lruCache) Get(key Key) (interface{}, bool) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	item, ok := l.items[key]
+	if !ok {
+		return nil, false
+	}
+
+	l.queue.MoveToFront(item)
+	cItem := item.Value.(cacheItem)
+
+	return cItem.value, true
+}
+
+func (l *lruCache) Clear() {
+	l.items = make(map[Key]*ListItem, l.capacity)
+	l.queue = NewList()
+}
+
+func (k Key) string() string {
+	return string(k)
 }
--- hw04_lru_cache/list_test.go
+++ hw04_lru_cache/list_test.go
@@ -18,13 +18,15 @@ func TestList(t *testing.T) {
 	t.Run("complex", func(t *testing.T) {
 		l := NewList()
 
-		l.PushFront(10) // [10]
-		l.PushBack(20)  // [10, 20]
-		l.PushBack(30)  // [10, 20, 30]
+		l.PushFront(10) // [<nil>][10][<nil>]
+		l.PushBack(20)  // [<nil>][10][20][<nil>]
+		l.PushBack(30)  // [<nil>][10][20][30][<nil>]
+
 		require.Equal(t, 3, l.Len())
 
-		middle := l.Front().Next // 20
+		middle := l.Front().Next // 30
 		l.Remove(middle)         // [10, 30]
+
 		require.Equal(t, 2, l.Len())
 
 		for i, v := range [...]int{40, 50, 60, 70, 80} {
@@ -46,6 +48,35 @@ func TestList(t *testing.T) {
 		for i := l.Front(); i != nil; i = i.Next {
 			elems = append(elems, i.Value.(int))
 		}
+
 		require.Equal(t, []int{70, 80, 60, 40, 10, 30, 50}, elems)
+
+		// проверим в обратном порядке (reverse)
+		elems = make([]int, 0, l.Len())
+		for i := l.Back(); i != nil; i = i.Prev {
+			elems = append(elems, i.Value.(int))
+		}
+
+		require.Equal(t, []int{50, 30, 10, 40, 60, 80, 70}, elems)
+	})
+
+	t.Run("remove all", func(t *testing.T) {
+		l := NewList()
+
+		// заполняем наш список
+		for i, v := range [...]int{40, 50, 60, 70, 80} {
+			if i%2 == 0 {
+				l.PushFront(v)
+			} else {
+				l.PushBack(v)
+			}
+		}
+
+		for i := l.Front(); i != nil; i = i.Next {
+			l.Remove(i)
+		}
+
+		require.Nil(t, l.Front())
+		require.Nil(t, l.Back())
 	})
 }
